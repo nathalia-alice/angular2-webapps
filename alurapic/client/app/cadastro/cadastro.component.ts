c5c91f0cd7ff93079fa1e5a1b0a489fac26cb743
nathalia-silva <nathalia.alice.silv@gmail.com>
2018-05-15 16:59:22 -0300

    envio de dados para o servidor

--- alurapic/client/app/cadastro/cadastro.component.ts
+++ alurapic/client/app/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input} from '@angular/core';
-import {FotoComponent} from '../foto/foto.component';
+import { Component, Input } from '@angular/core';
+import { FotoComponent } from '../foto/foto.component';
+import { Http, Headers } from '@angular/http';
 
 @Component({
     moduleId: module.id,
@@ -9,9 +10,22 @@ import {FotoComponent} from '../foto/foto.component';
 
 export class CadastroComponent { 
     foto: FotoComponent =  new FotoComponent();
+    http: Http;
+
+    constructor(http: Http){
+        this.http = http;
+    }
 
     cadastrar(event){
         event.preventDefault();
         console.log(this.foto);
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        this.http
+            .post('v1/fotos', JSON.stringify(this.foto), { headers: headers } )
+            .subscribe(() => {
+                this.foto = new FotoComponent();
+                console.log("Foto salva com sucesso");
+            }, erro => console.log(erro));
     }
 }
