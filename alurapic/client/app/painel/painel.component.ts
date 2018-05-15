cfdc2c3d355cf72f16923fc4259e723c9c990b2d
nathalia-silva <nathalia.alice.silv@gmail.com>
2018-05-15 17:06:28 -0300

    ciclo de vida de um componente

--- alurapic/client/app/painel/painel.component.ts
+++ alurapic/client/app/painel/painel.component.ts
@@ -1,11 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     moduleId: module.id,
     selector: 'painel',
     templateUrl: './painel.component.html'
 })
-export class PainelComponent {
+
+export class PainelComponent implements OnInit{
 
     @Input() titulo: string;
+
+    ngOnInit(){ //trás 7 caracteres no titulo da imagem
+        this.titulo = this.titulo.length > 7 ?
+        this.titulo.substr(0, 7) + '...' : 
+        this.titulo;
+    }
 }
