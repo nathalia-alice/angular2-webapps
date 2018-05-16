fad047344cfdd70cd27187834452b395c418e606
nathalia-silva <nathalia.alice.silv@gmail.com>
2018-05-16 15:21:18 -0300

    update foto

--- alurapic/client/app/foto/foto.service.ts
+++ alurapic/client/app/foto/foto.service.ts
@@ -25,8 +25,13 @@ export class FotoService {
 
     cadastra(foto: FotoComponent): Observable<Response> {
 
-        return this.http.post(this.url, JSON.stringify(foto), 
-                { headers: this.headers }); 
+            if (foto._id){
+                return this.http.put(this.url + '/' + foto._id, JSON.stringify(foto), 
+                { headers: this.headers });
+            }else{
+                return this.http.post(this.url, JSON.stringify(foto), 
+                { headers: this.headers });
+            }
 
     }
 
