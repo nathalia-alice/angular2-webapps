05abb43dd93282d979624029ae09bed13934eef6
nathalia-silva <nathalia.alice.silv@gmail.com>
2018-05-17 15:47:29 -0300

    remove comment

--- alurapic/client/app/app.module.ts
+++ alurapic/client/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { FotoModule } from './foto/foto.module';
-// importou o módulo que já possui um provider configurado
 import { HttpModule } from '@angular/http';
 import { PainelModule } from './painel/painel.module';
-
-import { CadastroComponent }   from './cadastro/cadastro.component'; // importou
-import { ListagemComponent }   from './listagem/listagem.component'; // importou
+import { CadastroComponent }   from './cadastro/cadastro.component';
+import { ListagemComponent }   from './listagem/listagem.component';
 import { routing } from './app.routes';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BotaoModule } from './botao/botao.module';
